b12fbb04b71f4b7ab57acf98ebabd5ca59b0de05
Gil Greenberg <gilgreenberg@gmail.com>
2022-04-04 15:58:38 -0400

    Surface session in middleware for internal API and response with example

--- server/routes/links/helpers/links.js
+++ server/routes/links/helpers/links.js
@@ -1,4 +1,20 @@
 export const all = async (req, res) => {
-  const { db } = req;
-  console.log("Inside all function");
+  const { db, session } = req;
+  console.log(`Inside all function for ${session.shop}`);
+
+  try {
+    const links = await db
+      .collection("links")
+      .find({
+        shop: session.shop,
+      })
+      .toArray();
+
+    return {
+      links: links || [],
+    };
+  } catch (err) {
+    // TODO:
+    throw err;
+  }
 };
