da409840965bf8bfda9c73b0da892f99d8b25cce
Gil Greenberg <gilgreenberg@gmail.com>
2022-04-04 15:58:49 -0400

    Populate example links

--- server/routes/links/helpers/create.js
+++ server/routes/links/helpers/create.js
@@ -1,9 +1,11 @@
 // Add payload into Mongodb
 export const create = async (req, res) => {
-  const { db, analytics } = req;
+  const { db, analytics, session } = req;
+  const shop = session.shop;
 
   try {
     await db.collection("links").insertOne({
+      shop: shop,
       name: "example link",
       alias: "123abc", // TODO: unique index
       type: "checkout", // ["cart", "checkout"]
@@ -11,8 +13,17 @@ export const create = async (req, res) => {
     });
     console.log("Inside create function");
 
+    // TODO: check if created successfully otherwise throw
+
+    analytics &&
+      analytics.track({
+        event: "link created",
+        userId: shop,
+      });
+
     // TODO: fire event? analytics.create({ })
   } catch (err) {
     // TODO:
+    res.status(500).send("");
   }
 };
