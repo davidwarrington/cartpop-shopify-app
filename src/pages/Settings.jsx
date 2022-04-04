e4550bf508968f1947620cda2bb5baf398cffcc7
Gil Greenberg <gilgreenberg@gmail.com>
2022-04-04 19:30:45 -0400

    Add example title bar implementation

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,11 +1,24 @@
 import { Page } from "@shopify/polaris";
 import { useNavigate } from "react-router-dom";
+import { TitleBar } from "@shopify/app-bridge-react";
 
 const Settings = () => {
   const navigate = useNavigate();
 
+  const primaryAction = { content: "Foo", url: "/foo" };
+  const secondaryActions = [{ content: "Bar", url: "/bar", loading: true }];
+  const actionGroups = [
+    { title: "Baz", actions: [{ content: "Baz", url: "/baz" }] },
+  ];
+
   return (
     <Page breadcrumbs={[{ content: "Home", url: "/" }]} title="Settings">
+      <TitleBar
+        title="Settings"
+        primaryAction={primaryAction}
+        secondaryActions={secondaryActions}
+        actionGroups={actionGroups}
+      />
       settings...
     </Page>
   );
