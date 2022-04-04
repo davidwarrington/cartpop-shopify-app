7f650613ecb6134dffd8bb56f8c36d30882077db
Gil Greenberg <gilgreenberg@gmail.com>
2022-04-03 21:42:00 -0400

    Wrap express app with mongodb connection and include mongodb sessions

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 import { resolve } from "path";
 import express from "express";
 import cookieParser from "cookie-parser";
+import { MongoClient } from "mongodb";
 import { Shopify, ApiVersion } from "@shopify/shopify-api";
 import "dotenv/config";
 
@@ -15,6 +16,52 @@ const TOP_LEVEL_OAUTH_COOKIE = "shopify_top_level_oauth";
 const PORT = parseInt(process.env.PORT || "8081", 10);
 const isTest = process.env.NODE_ENV === "test" || !!process.env.VITE_TEST_BUILD;
 
+//////////////////////
+// MOGNODB Sessions
+let mongodb;
+const { MONGODB_DB, MONGODB_URI } = process.env;
+
+async function storeCallback(session) {
+  try {
+    await mongodb
+      .db(MONGODB_DB)
+      .collection("__session")
+      .updateOne({ id: session.id }, { $set: session }, { upsert: true });
+    return true;
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+
+async function loadCallback(id) {
+  try {
+    const mongoSession = await mongodb
+      .db(MONGODB_DB)
+      .collection("__session")
+      .findOne({ id: id });
+
+    if (!mongoSession) return false;
+
+    let session = new Shopify.Session.Session(mongoSession.id);
+    session = Object.assign(session, mongoSession);
+    return session;
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+
+async function deleteCallback(id) {
+  try {
+    return await mongodb
+      .db(MONGODB_DB)
+      .collection("__session")
+      .deleteOne({ id: id });
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+//////////////////////
+
 Shopify.Context.initialize({
   API_KEY: process.env.SHOPIFY_API_KEY,
   API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
@@ -22,8 +69,11 @@ Shopify.Context.initialize({
   HOST_NAME: process.env.HOST.replace(/https:\/\//, ""),
   API_VERSION: ApiVersion.April22,
   IS_EMBEDDED_APP: true,
-  // This should be replaced with your preferred storage strategy
-  SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
+  SESSION_STORAGE: new Shopify.Session.CustomSessionStorage(
+    storeCallback,
+    loadCallback,
+    deleteCallback
+  ),
 });
 
 // Storing the currently active shops in memory will force them to re-login when your server restarts. You should
@@ -32,7 +82,7 @@ const ACTIVE_SHOPIFY_SHOPS = {};
 Shopify.Webhooks.Registry.addHandler("APP_UNINSTALLED", {
   path: "/webhooks",
   webhookHandler: async (topic, shop, body) => {
-    delete ACTIVE_SHOPIFY_SHOPS[shop]
+    delete ACTIVE_SHOPIFY_SHOPS[shop];
   },
 });
 
@@ -42,112 +92,122 @@ export async function createServer(
   isProd = process.env.NODE_ENV === "production"
 ) {
   const app = express();
-  app.set("top-level-oauth-cookie", TOP_LEVEL_OAUTH_COOKIE);
-  app.set("active-shopify-shops", ACTIVE_SHOPIFY_SHOPS);
-  app.set("use-online-tokens", USE_ONLINE_TOKENS);
-
-  app.use(cookieParser(Shopify.Context.API_SECRET_KEY));
-
-  applyAuthMiddleware(app);
-
-  app.post("/webhooks", async (req, res) => {
-    try {
-      await Shopify.Webhooks.Registry.process(req, res);
-      console.log(`Webhook processed, returned status code 200`);
-    } catch (error) {
-      console.log(`Failed to process webhook: ${error}`);
-      res.status(500).send(error.message);
-    }
-  });
-
-  app.get("/products-count", verifyRequest(app), async (req, res) => {
-    const session = await Shopify.Utils.loadCurrentSession(req, res, true);
-    const { Product } = await import(
-      `@shopify/shopify-api/dist/rest-resources/${Shopify.Context.API_VERSION}/index.js`
-    );
-
-    const countData = await Product.count({ session });
-    res.status(200).send(countData);
-  });
-
-  app.post("/graphql", verifyRequest(app), async (req, res) => {
-    try {
-      const response = await Shopify.Utils.graphqlProxy(req, res);
-      res.status(200).send(response.body);
-    } catch (error) {
-      res.status(500).send(error.message);
-    }
-  });
-
-  app.use(express.json());
-
-  webhookGdprRoutes(app);
-
-  app.use((req, res, next) => {
-    const shop = req.query.shop;
-    if (Shopify.Context.IS_EMBEDDED_APP && shop) {
-      res.setHeader(
-        "Content-Security-Policy",
-        `frame-ancestors https://${shop} https://admin.shopify.com;`
-      );
-    } else {
-      res.setHeader("Content-Security-Policy", `frame-ancestors 'none';`);
-    }
-    next();
-  });
-
-  app.use("/*", (req, res, next) => {
-    const shop = req.query.shop;
-
-    // Detect whether we need to reinstall the app, any request from Shopify will
-    // include a shop in the query parameters.
-    if (app.get("active-shopify-shops")[shop] === undefined && shop) {
-      res.redirect(`/auth?shop=${shop}`);
-    } else {
-      next();
-    }
-  });
-
   /**
    * @type {import('vite').ViteDevServer}
    */
   let vite;
-  if (!isProd) {
-    vite = await import("vite").then(({ createServer }) =>
-      createServer({
-        root,
-        logLevel: isTest ? "error" : "info",
-        server: {
-          port: PORT,
-          hmr: {
-            protocol: "ws",
-            host: "localhost",
-            port: 64999,
-            clientPort: 64999,
-          },
-          middlewareMode: "html",
-        },
-      })
-    );
-    app.use(vite.middlewares);
-  } else {
-    const compression = await import("compression").then(
-      ({ default: fn }) => fn
-    );
-    const serveStatic = await import("serve-static").then(
-      ({ default: fn }) => fn
-    );
-    const fs = await import("fs");
-    app.use(compression());
-    app.use(serveStatic(resolve("dist/client")));
-    app.use("/*", (req, res, next) => {
-      // Client-side routing will pick up on the correct route to render, so we always render the index here
-      res
-        .status(200)
-        .set("Content-Type", "text/html")
-        .send(fs.readFileSync(`${process.cwd()}/dist/client/index.html`));
+
+  // Connect to Mongodb and wrap app for connection pooling
+  MongoClient.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(async (connection) => {
+    console.log(`Successfully connected to ${MONGODB_DB}`);
+    mongodb = connection;
+
+    app.set("top-level-oauth-cookie", TOP_LEVEL_OAUTH_COOKIE);
+    app.set("active-shopify-shops", ACTIVE_SHOPIFY_SHOPS);
+    app.set("use-online-tokens", USE_ONLINE_TOKENS);
+
+    app.use(cookieParser(Shopify.Context.API_SECRET_KEY));
+
+    applyAuthMiddleware(app);
+
+    app.post("/webhooks", async (req, res) => {
+      try {
+        await Shopify.Webhooks.Registry.process(req, res);
+        console.log(`Webhook processed, returned status code 200`);
+      } catch (error) {
+        console.log(`Failed to process webhook: ${error}`);
+        res.status(500).send(error.message);
+      }
     });
-  }
+
+    app.get("/products-count", verifyRequest(app), async (req, res) => {
+      const session = await Shopify.Utils.loadCurrentSession(req, res, true);
+      const { Product } = await import(
+        `@shopify/shopify-api/dist/rest-resources/${Shopify.Context.API_VERSION}/index.js`
+      );
+
+      const countData = await Product.count({ session });
+      res.status(200).send(countData);
+    });
+
+    app.post("/graphql", verifyRequest(app), async (req, res) => {
+      try {
+        const response = await Shopify.Utils.graphqlProxy(req, res);
+        res.status(200).send(response.body);
+      } catch (error) {
+        res.status(500).send(error.message);
+      }
+    });
+
+    app.use(express.json());
+
+    webhookGdprRoutes(app);
+
+    app.use((req, res, next) => {
+      const shop = req.query.shop;
+      if (Shopify.Context.IS_EMBEDDED_APP && shop) {
+        res.setHeader(
+          "Content-Security-Policy",
+          `frame-ancestors https://${shop} https://admin.shopify.com;`
+        );
+      } else {
+        res.setHeader("Content-Security-Policy", `frame-ancestors 'none';`);
+      }
+      next();
+    });
+
+    app.use("/*", (req, res, next) => {
+      const shop = req.query.shop;
+
+      // Detect whether we need to reinstall the app, any request from Shopify will
+      // include a shop in the query parameters.
+      if (app.get("active-shopify-shops")[shop] === undefined && shop) {
+        res.redirect(`/auth?shop=${shop}`);
+      } else {
+        next();
+      }
+    });
+
+    if (!isProd) {
+      vite = await import("vite").then(({ createServer }) =>
+        createServer({
+          root,
+          logLevel: isTest ? "error" : "info",
+          server: {
+            port: PORT,
+            hmr: {
+              protocol: "ws",
+              host: "localhost",
+              port: 64999,
+              clientPort: 64999,
+            },
+            middlewareMode: "html",
+          },
+        })
+      );
+      app.use(vite.middlewares);
+    } else {
+      const compression = await import("compression").then(
+        ({ default: fn }) => fn
+      );
+      const serveStatic = await import("serve-static").then(
+        ({ default: fn }) => fn
+      );
+      const fs = await import("fs");
+      app.use(compression());
+      app.use(serveStatic(resolve("dist/client")));
+      app.use("/*", (req, res, next) => {
+        // Client-side routing will pick up on the correct route to render, so we always render the index here
+        res
+          .status(200)
+          .set("Content-Type", "text/html")
+          .send(fs.readFileSync(`${process.cwd()}/dist/client/index.html`));
+      });
+    }
+  });
 
   return { app, vite };
 }
