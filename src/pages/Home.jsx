873af63447c960b2417966f64c58fa509a453048
Gil Greenberg <gilgreenberg@gmail.com>
2022-04-04 22:37:07 -0400

    Add empty state for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,30 @@
 import { useEffect, useState } from "react";
-import { Banner, Button, Layout, Link, Page } from "@shopify/polaris";
+import {
+  Banner,
+  Button,
+  Card,
+  EmptyState,
+  Layout,
+  Link,
+  Page,
+} from "@shopify/polaris";
 import { useAppBridge, TitleBar } from "@shopify/app-bridge-react";
 import { authenticatedFetch } from "@shopify/app-bridge-utils";
 
-import { CheckoutLinkCard } from "../components/CheckoutLinkCard";
-import { ProductsCard } from "../components/ProductsCard";
-import { CustomerCard } from "../components/CustomerCard";
-import { OrderCard } from "../components/OrderCard";
+import cashRegister from "../assets/cash_register_128.png";
 
 const Home = () => {
   const app = useAppBridge();
 
-  const [products, setProducts] = useState([]);
-  const [customer, setCustomer] = useState({});
-  const [order, setOrder] = useState({});
+  const [pageState, setPageState] = useState(); // TODO:
+  const [links, setLinks] = useState([]);
 
   async function getLinks() {
     const fetchFunction = authenticatedFetch(app);
     const apiRes = await fetchFunction("/api/links").then((res) => res.json());
     console.log("apiRes", apiRes);
+
+    //setLinks()
   }
 
   useEffect(() => {
@@ -30,8 +36,31 @@ const Home = () => {
       {/* Empty TitleBar to reset when navigating from other pages like Settings */}
       <TitleBar />
       <Layout>
-        <Layout.Section fullWidth>
-          Links here...
+        {links && links.length ? (
+          <Layout.Section fullWidth>
+            <Button url="/links/123">123 Link</Button>
+          </Layout.Section>
+        ) : (
+          <Layout.Section fullWidth>
+            <Card>
+              <EmptyState
+                image={cashRegister}
+                heading="Create checkout links"
+                action={{
+                  content: "Create new link",
+                  url: "/links/new",
+                }}
+              >
+                <p>
+                  Create and manage one click checkout links that automatically
+                  add products to a customer's cart and navigate directly to
+                  checkout.
+                </p>
+              </EmptyState>
+            </Card>
+          </Layout.Section>
+        )}
+        <Layout.Section>
           <br />
           <Banner>
             Made in New York City by{" "}
@@ -40,11 +69,6 @@ const Home = () => {
             </Link>
             .
           </Banner>
-        </Layout.Section>
-        <Layout.Section fullWidth>
-          <Button url="/links/new">New link</Button>
-          <Button url="/links/123">123 Link</Button>
-          <Button url="/settings">Settings</Button>
         </Layout.Section>
       </Layout>
     </Page>
