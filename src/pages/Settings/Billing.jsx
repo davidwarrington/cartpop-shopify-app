f705c7b439e6b304d85ce9f4971d6fa3995eda33
Gil Greenberg <gilgreenberg@gmail.com>
2022-04-10 17:12:56 -0400

    Add billing page within client app

--- src/pages/Settings/Billing.jsx
+++ src/pages/Settings/Billing.jsx
@@ -1,11 +1,51 @@
-import { Card, Page } from "@shopify/polaris";
-import { useNavigate } from "react-router-dom";
-import { TitleBar } from "@shopify/app-bridge-react";
+import { useCallback } from "react";
+import {
+  Card,
+  EmptyState,
+  FooterHelp,
+  Heading,
+  Icon,
+  Image,
+  Link,
+  Page,
+  Stack,
+  TextStyle,
+} from "@shopify/polaris";
+import { TitleBar, useAppBridge } from "@shopify/app-bridge-react";
+import { Redirect } from "@shopify/app-bridge/actions";
+import { TickMinor } from "@shopify/polaris-icons";
+import { userLoggedInFetch } from "../../helpers";
+
+import UpgradeIcon from "../../assets/icon-upgrade.png";
+import BenefitUnlimited from "../../assets/benefit-unlimited.png";
+import BenefitAnalytics from "../../assets/benefit-analytics.png";
+import BenefitAlias from "../../assets/benefit-alias.png";
+import BenefitReorder from "../../assets/benefit-reorder.png";
 
 const SettingsBillingPage = () => {
-  const navigate = useNavigate();
+  const app = useAppBridge();
+  const redirect = Redirect.create(app);
+  const fetchFunction = userLoggedInFetch(app);
 
   const pageTitle = "Billing";
+
+  const handleSubscription = useCallback(async () => {
+    try {
+      const result = await fetchFunction(`/api/billing`, {
+        method: "POST",
+      }).then((res) => res.json());
+
+      if (!result || !result.url) {
+        throw `Plan change was unsuccessful`;
+      }
+
+      // Redirect to admin billing approval screen
+      redirect.dispatch(Redirect.Action.REMOTE, result.url);
+    } catch (err) {
+      // TODO: send to bugsnag
+      console.warn(err);
+    }
+  }, []);
 
   return (
     <Page
@@ -22,7 +62,135 @@ const SettingsBillingPage = () => {
           { content: "Settings", url: "/settings" },
         ]}
       />
-      <Card sectioned></Card>
+      <Card>
+        <Card.Section>
+          <EmptyState
+            // TODO: replace image
+            image={UpgradeIcon}
+            heading="Upgrade to Pro for $10/mo"
+            action={{
+              content: "Upgrade to Pro",
+              onAction: handleSubscription,
+            }}
+          >
+            <Stack>
+              <Stack.Item>
+                Upgrade to unlock all features including link aliases and
+                analytics.
+              </Stack.Item>
+            </Stack>
+          </EmptyState>
+        </Card.Section>
+        <Card.Section subdued>
+          <Stack distribution="center">
+            <Heading>Why Upgrade to Pro?</Heading>
+          </Stack>
+          <br />
+          <div
+            style={{
+              display: "grid",
+              gridTemplateColumns: "repeat(3, 1fr)",
+              gridGap: "2rem",
+            }}
+          >
+            <Stack.Item fill>
+              <Stack vertical spacing="tight" distribution="center">
+                <Stack
+                  vertical
+                  distribution="center"
+                  alignment="center"
+                  spacing="extraTight"
+                >
+                  <Image source={BenefitUnlimited} />
+                  <Stack alignment="center" spacing="tight">
+                    <Icon source={TickMinor} color="success" />
+                    <Heading>Create unlimited links</Heading>
+                  </Stack>
+                </Stack>
+                <Stack alignment="center" wrap={false}>
+                  <div style={{ textAlign: "center" }}>
+                    Create as many links as you need with no limitations.
+                  </div>
+                </Stack>
+              </Stack>
+            </Stack.Item>
+            <Stack.Item fill>
+              <Stack vertical spacing="tight" distribution="center">
+                <Stack
+                  vertical
+                  distribution="center"
+                  alignment="center"
+                  spacing="extraTight"
+                >
+                  <Image source={BenefitAlias} />
+                  <Stack alignment="center" spacing="tight">
+                    <Icon source={TickMinor} color="success" />
+                    <Heading>Link aliases</Heading>
+                  </Stack>
+                  <Stack alignment="center" distribution="center" wrap={false}>
+                    <div style={{ textAlign: "center" }}>
+                      Share with customers memorable short checkout links with
+                      link aliases.
+                    </div>
+                  </Stack>
+                </Stack>
+              </Stack>
+            </Stack.Item>
+            <Stack.Item fill>
+              <Stack vertical spacing="tight" distribution="center">
+                <Stack
+                  vertical
+                  distribution="center"
+                  alignment="center"
+                  spacing="extraTight"
+                >
+                  <Image source={BenefitAnalytics} />
+                  <Stack alignment="center" spacing="tight">
+                    <Icon source={TickMinor} color="success" />
+                    <Heading>Link analytics</Heading>
+                  </Stack>
+                </Stack>
+                <Stack distribution="center" wrap={false}>
+                  <div style={{ textAlign: "center" }}>
+                    Get detailed analytics including clicks, qr code scans, and
+                    more.
+                  </div>
+                </Stack>
+              </Stack>
+            </Stack.Item>
+
+            {/* <Stack vertical>
+              <Image source={UpgradeIcon} />
+              <Stack alignment="center" >
+                <Icon source={MobileAcceptMajor} color="success" />
+                <Stack.Item>
+                  <Stack vertical spacing="none">
+                    <Heading>Excellent support</Heading>
+                    <Stack.Item>Quick and responsive chat &amp; email support.</Stack.Item>
+                  </Stack>
+                </Stack.Item>
+              </Stack>
+            </Stack> */}
+
+            {/* <Stack alignment="center" spacing="">
+              <Icon source={MobileAcceptMajor} color="success" />
+              <Stack.Item>
+                <Stack vertical spacing="none">
+                  <Heading>Reorder links</Heading>
+                  <Stack.Item>Easily allow customers to reorder products they've previously purchased.</Stack.Item>
+                </Stack>
+              </Stack.Item>
+            </Stack> */}
+          </div>
+          <br />
+        </Card.Section>
+      </Card>
+      <FooterHelp>
+        <TextStyle variation="subdued">
+          All pricing is shown in USD. Learn more about <Link>CartPop Pro</Link>
+          .
+        </TextStyle>
+      </FooterHelp>
     </Page>
   );
 };
