f07d83572d4b470e97d833619514e2a02736a505
Gil Greenberg <gilgreenberg@gmail.com>
2022-04-17 18:38:56 -0400

    Add French translations

--- server/default-translations.js
+++ server/default-translations.js
@@ -3,4 +3,9 @@ export const defaultTranslations = {
     loading_title: "Loading checkout",
     loading_message: "Please wait while we load your checkout.",
   },
+  fr: {
+    loading_title: "Checkout en chargement",
+    loading_message:
+      "Merci d'attendre le temps que l'on charge votre checkout.",
+  },
 };
