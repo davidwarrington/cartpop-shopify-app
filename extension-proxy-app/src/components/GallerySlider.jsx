16cd9e6680a4c112b6542d829ca8f929448fdff9
Gil Greenberg <gilgreenberg@gmail.com>
2022-04-26 17:41:10 -0400

    Add proxy page app

---
+++ extension-proxy-app/src/components/GallerySlider.jsx
@@ -0,0 +1,34 @@
+const GallerySlider = ({ product }) => {
+  return (
+    <div className="relative overflow-auto pt-2 bg-gray-50">
+      <div className="relative w-full flex gap-6 snap-mandatory snap-x overflow-x-auto pb-2">
+        <div className="snap-center shrink-0 first:pl-4 last:pr-8">
+          <img
+            className="shrink-0 w-72 h-72 rounded-lg bg-white border"
+            src={product.featured_image}
+          />
+        </div>
+        <div className="snap-center shrink-0 first:pl-8 last:pr-8">
+          <img
+            className="shrink-0 w-72 h-72 rounded-lg bg-white border"
+            src={product.featured_image}
+          />
+        </div>
+        <div className="snap-center shrink-0 first:pl-8 last:pr-8">
+          <img
+            className="shrink-0 w-72 h-72 rounded-lg bg-white border"
+            src={product.featured_image}
+          />
+        </div>
+        <div className="snap-center shrink-0 first:pl-8 last:pr-8">
+          <img
+            className="shrink-0 w-72 h-72 rounded-lg bg-white border"
+            src={product.featured_image}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GallerySlider;
---
+++ extension-proxy-app/src/components/index.js
@@ -0,0 +1,6 @@
+import CheckoutActions from "./CheckoutActions";
+import ModalContainer from "./ModalContainer";
+import GallerySlider from "./GallerySlider";
+import QRCode from "./QRCode";
+
+export { CheckoutActions, GallerySlider, ModalContainer, QRCode };
