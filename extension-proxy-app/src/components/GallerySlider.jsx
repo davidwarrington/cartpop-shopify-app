4aec6ce74edbc80b6dbbee0229353e69a498b1bf
Gil Greenberg <gilgreenberg@gmail.com>
2022-04-28 22:31:43 -0400

    dynamically load images

--- extension-proxy-app/src/components/GallerySlider.jsx
+++ extension-proxy-app/src/components/GallerySlider.jsx
@@ -5,31 +5,20 @@ const GallerySlider = ({ product }) => {
 
   return (
     <div className="relative overflow-auto pt-2 bg-gray-50">
-      <div className="relative w-full flex gap-6 snap-mandatory snap-x overflow-x-auto pb-2">
-        <div className="snap-center shrink-0 first:pl-4 last:pr-8">
-          <img
-            className="shrink-0 w-72 h-72 rounded-lg bg-white border"
-            src={product.featured_image}
-          />
-        </div>
-        <div className="snap-center shrink-0 first:pl-8 last:pr-8">
-          <img
-            className="shrink-0 w-72 h-72 rounded-lg bg-white border"
-            src={product.featured_image}
-          />
-        </div>
-        <div className="snap-center shrink-0 first:pl-8 last:pr-8">
-          <img
-            className="shrink-0 w-72 h-72 rounded-lg bg-white border"
-            src={product.featured_image}
-          />
-        </div>
-        <div className="snap-center shrink-0 first:pl-8 last:pr-8">
-          <img
-            className="shrink-0 w-72 h-72 rounded-lg bg-white border"
-            src={product.featured_image}
-          />
-        </div>
+      <div className="relative w-full flex gap-6 snap-mandatory snap-x overflow-x-auto pb-2 pl-4">
+        {product.images.map((image, imageIndex) => (
+          <div
+            className={`snap-center shrink-0 first:pl-${
+              imageIndex === 0 ? "4" : "8"
+            } last:pr-8`}
+          >
+            <img
+              className="shrink-0 w-60 h-60 rounded-lg bg-white border"
+              src={image}
+              alt=""
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
