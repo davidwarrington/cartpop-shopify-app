c36b8bac6c5bb73d5dbb40c4576c3308ee893286
Gil Greenberg <gilgreenberg@gmail.com>
2022-04-29 16:14:39 -0400

    Add brand color variable using css var

--- extension-proxy-app/tailwind.config.js
+++ extension-proxy-app/tailwind.config.js
@@ -1,7 +1,20 @@
+function withOpacityValue(variable) {
+  return ({ opacityValue }) => {
+    if (opacityValue === undefined) {
+      return `rgb(var(${variable}))`;
+    }
+    return `rgb(var(${variable}) / ${opacityValue})`;
+  };
+}
+
 module.exports = {
   content: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: withOpacityValue("--color-brand"),
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
