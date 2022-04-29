5c9128c47fca7597cf12da5fa459807a403a3f0a
Gil Greenberg <gilgreenberg@gmail.com>
2022-04-28 22:32:07 -0400

    Add dynamic variant selector

--- extension-proxy-app/src/components/VariantSelector.jsx
+++ extension-proxy-app/src/components/VariantSelector.jsx
@@ -1,4 +1,147 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment, useState } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, SelectorIcon } from "@heroicons/react/solid";
+
+const people = [
+  {
+    id: 1,
+    name: "Wade Cooper",
+    avatar:
+      "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    id: 2,
+    name: "Arlene Mccoy",
+    avatar:
+      "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    id: 3,
+    name: "Devon Webb",
+    avatar:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80",
+  },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
 const VariantSelector = ({ product }) => {
+  const options = product.variants.map((variant) => ({
+    label: variant.options[0],
+  }));
+
+  const [selected, setSelected] = useState(options[0]);
+
+  return (
+    <div className="w-full min-w-[50%]">
+      <label
+        htmlFor="location"
+        className="hidden block text-sm font-medium text-gray-700"
+      >
+        {product.options[0]}
+      </label>
+      <select
+        id="location"
+        name="location"
+        className="w-full h-14 block text-base sm:text-sm bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+        //defaultValue="Canada"
+      >
+        {options.map((option) => (
+          <option>{option.label}</option>
+        ))}
+      </select>
+    </div>
+  );
+
+  return (
+    <Listbox value={selected} onChange={setSelected}>
+      {({ open }) => (
+        <>
+          <Listbox.Label className="hidden block text-sm font-medium text-gray-700">
+            {product.options[0]}
+          </Listbox.Label>
+          <div className="relative">
+            <Listbox.Button className="relative w-full h-14 bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+              <span className="flex items-center">
+                {selected.featured_image ? (
+                  <img
+                    src={selected.featured_image}
+                    alt=""
+                    className="flex-shrink-0 h-6 w-6 rounded-full"
+                  />
+                ) : null}
+                <span className="ml-3 block truncate">{selected.label}</span>
+              </span>
+              <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                <SelectorIcon
+                  className="h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </span>
+            </Listbox.Button>
+
+            <Transition
+              show={open}
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+                {options.map((option) => (
+                  <Listbox.Option
+                    key={option.id}
+                    className={({ active }) =>
+                      classNames(
+                        active ? "text-white bg-indigo-600" : "text-gray-900",
+                        "cursor-default select-none relative py-2 pl-3 pr-9"
+                      )
+                    }
+                    value={option}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <div className="flex items-center">
+                          <img
+                            src={person.avatar}
+                            alt=""
+                            className="flex-shrink-0 h-6 w-6 rounded-full"
+                          />
+                          <span
+                            className={classNames(
+                              selected ? "font-semibold" : "font-normal",
+                              "ml-3 block truncate"
+                            )}
+                          >
+                            {person.name}
+                          </span>
+                        </div>
+
+                        {selected ? (
+                          <span
+                            className={classNames(
+                              active ? "text-white" : "text-indigo-600",
+                              "absolute inset-y-0 right-0 flex items-center pr-4"
+                            )}
+                          >
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        </>
+      )}
+    </Listbox>
+  );
+
   return (
     <button
       type="button"
