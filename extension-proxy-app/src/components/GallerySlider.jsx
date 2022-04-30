ea7132bb14e7863427caeab88690217dbd3f0276
Gil Greenberg <gilgreenberg@gmail.com>
2022-04-30 19:15:27 -0400

    For single image, don't use gallery or hide completel if product has no images

--- extension-proxy-app/src/components/GallerySlider.jsx
+++ extension-proxy-app/src/components/GallerySlider.jsx
@@ -3,6 +3,24 @@ const GallerySlider = ({ product }) => {
     return null;
   }
 
+  if (!product.images || !product.images.length) {
+    return null;
+  }
+
+  if (product.images.length === 1) {
+    return (
+      <div className="relative bg-gray-50 flex justify-center">
+        <div className="p-5">
+          <img
+            className="w-64 h-64 rounded-lg bg-white border object-contain"
+            src={product.images[0]}
+            alt=""
+          />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="relative overflow-auto pt-2 bg-gray-50">
       <div className="relative w-full flex gap-6 snap-mandatory snap-x overflow-x-auto pb-2 pl-4">
@@ -13,7 +31,7 @@ const GallerySlider = ({ product }) => {
             } last:pr-8`}
           >
             <img
-              className="shrink-0 w-60 h-60 rounded-lg bg-white border"
+              className="shrink-0 w-60 h-60 rounded-lg bg-white border object-contain"
               src={image}
               alt=""
             />
