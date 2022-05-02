d503c323ffaf9e7555ad14161a616f046d523ac3
Gil Greenberg <gilgreenberg@gmail.com>
2022-05-02 14:23:25 -0400

    Export new components

--- extension-proxy-app/src/components/index.js
+++ extension-proxy-app/src/components/index.js
@@ -2,6 +2,8 @@ import CheckoutActions from "./CheckoutActions";
 import ModalContainer from "./ModalContainer";
 import GallerySlider from "./GallerySlider";
 import Product from "./Product";
+import ProductPrice from "./ProductPrice";
+import ProductRating from "./ProductRating";
 import QRCode from "./QRCode";
 import QuantityStepper from "./QuantityStepper";
 import VariantSelector from "./VariantSelector";
@@ -11,6 +13,8 @@ export {
   GallerySlider,
   ModalContainer,
   Product,
+  ProductRating,
+  ProductPrice,
   QRCode,
   QuantityStepper,
   VariantSelector,
