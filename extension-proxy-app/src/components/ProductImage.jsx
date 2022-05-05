b3bed63576a3f430802689756be935d7ecfb84d4
Gil Greenberg <gilgreenberg@gmail.com>
2022-05-05 18:52:18 -0400

    Add standalone product image component

---
+++ extension-proxy-app/src/components/ProductImage.jsx
@@ -0,0 +1,17 @@
+const ProductImage = ({ product }) => {
+  if (!product.images || !product.images.length) {
+    return <div className="w-50 bg-slate-500" />;
+  }
+
+  return (
+    <>
+      <img
+        className="h-full aspect-square object-cover w-1/3 rounded-2xl p-2 box-border"
+        src={product.images[0].src}
+        alt=""
+      />
+    </>
+  );
+};
+
+export default ProductImage;
