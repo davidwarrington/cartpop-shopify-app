f8ace829496e3dd8720c81703fdb4c683e715697
Gil Greenberg <gilgreenberg@gmail.com>
2022-05-05 12:54:23 -0400

    Always show top bar and default to store name

--- extension-proxy-app/src/components/TopBar.jsx
+++ extension-proxy-app/src/components/TopBar.jsx
@@ -1,17 +1,17 @@
 import { useShop } from "../hooks";
 
 const TopBar = () => {
-  const { logo_url } = useShop();
-
-  if (!logo_url) {
-    return null;
-  }
+  const { logo_url, name } = useShop();
 
   return (
     <div className="bg-white rounded-t-lg">
-      <div className="relative flex justify-center py-2 border-b">
-        <div className="h-10">
-          <img className="h-full" src={logo_url} alt="" />
+      <div className="relative flex justify-center py-2">
+        <div className="h-10 flex items-center font-medium">
+          {logo_url ? (
+            <img className="h-full" src={logo_url} alt="" />
+          ) : (
+            <h1>{name}</h1>
+          )}
         </div>
       </div>
     </div>
