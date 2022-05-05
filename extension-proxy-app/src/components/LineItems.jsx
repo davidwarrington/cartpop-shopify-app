f76f2cc5a21cd5caf90e36a84e1b78422adf4366
Gil Greenberg <gilgreenberg@gmail.com>
2022-05-05 12:55:33 -0400

    Fetch helper function from cart hook

--- extension-proxy-app/src/components/LineItems.jsx
+++ extension-proxy-app/src/components/LineItems.jsx
@@ -14,7 +14,6 @@ const LineItems = ({ products }) => {
       lineItem={lineItem}
       lineItemIndex={lineItemIndex}
       products={products}
-      handleLineItemChange={handleLineItemChange}
     />
   ));
 };
