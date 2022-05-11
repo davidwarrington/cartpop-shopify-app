fa24b751d17d9c0ddb2ae7412bdcd28eeee9b15c
Gil Greenberg <gilgreenberg@gmail.com>
2022-05-10 22:35:22 -0400

    Switch to Indextable

--- src/components/AllLinksTable.jsx
+++ src/components/AllLinksTable.jsx
@@ -1,7 +1,23 @@
-import { Badge, Card, DataTable, Link, TextStyle } from "@shopify/polaris";
+import {
+  Badge,
+  Card,
+  IndexTable,
+  Link,
+  Stack,
+  TextStyle,
+  useIndexResourceState,
+} from "@shopify/polaris";
 import { capitalize } from "../helpers";
 
+const resourceName = {
+  singular: "link",
+  plural: "links",
+};
+
 export function AllLinksCard({ links }) {
+  const { selectedResources, allResourcesSelected, handleSelectionChange } =
+    useIndexResourceState(links);
+
   if (!links || !links.length) {
     return null;
   }
@@ -19,26 +35,72 @@ export function AllLinksCard({ links }) {
       scans += link.analytics.scans.desktop || 0;
     }
 
-    return [
-      <Link url={`/links/${link._id}`}>{link.name || link._id}</Link>,
-      capitalize(link.type),
-      link.active ? (
-        <Badge status="success">Enabled</Badge>
-      ) : (
-        <TextStyle variation="subdued">Disabled</TextStyle>
-      ),
-      clicks,
-      scans,
-    ];
+    return {
+      id: link._id,
+      url: `links/${link._id}`,
+      active: link.active,
+      name: link.name || link._id,
+      destination: (link.settings && link.settings.destination) || null,
+      clearCart: (link.settings && link.settings.clearCart) || false,
+      clicks: clicks,
+      scans: scans,
+    };
   });
+
+  const rowMarkup = rows.map(
+    (
+      { id, name, url, destination, active, clearCart, clicks, scans },
+      index
+    ) => (
+      <IndexTable.Row
+        id={id}
+        key={id}
+        //selected={selectedResources.includes(id)}
+        position={index}
+      >
+        <IndexTable.Cell>
+          <Link plain removeUnderline monochrome url={url}>
+            <Stack vertical spacing="extraTight">
+              <TextStyle variation="strong">{name}</TextStyle>
+              <TextStyle>
+                {capitalize(destination)} •{" "}
+                {clearCart ? "Clear cart" : "Don't clear cart"}{" "}
+              </TextStyle>
+            </Stack>
+          </Link>
+        </IndexTable.Cell>
+        <IndexTable.Cell>
+          {active ? (
+            <Badge status="success">Enabled</Badge>
+          ) : (
+            <TextStyle variation="subdued">Disabled</TextStyle>
+          )}
+        </IndexTable.Cell>
+        <IndexTable.Cell>{clicks}</IndexTable.Cell>
+        <IndexTable.Cell>{scans}</IndexTable.Cell>
+      </IndexTable.Row>
+    )
+  );
 
   return (
     <Card>
-      <DataTable
-        columnContentTypes={["text", "text", "text", "numeric", "numeric"]}
-        headings={["Link", "Type", "Status", "Clicks", "Scans"]}
-        rows={rows}
-      />
+      <IndexTable
+        resourceName={resourceName}
+        itemCount={rows.length}
+        // selectedItemsCount={
+        //   allResourcesSelected ? 'All' : selectedResources.length
+        // }
+        selectable={false}
+        onSelectionChange={handleSelectionChange}
+        headings={[
+          { title: "Name" },
+          { title: "Active" },
+          { title: "Clicks" },
+          { title: "Scans" },
+        ]}
+      >
+        {rowMarkup}
+      </IndexTable>
     </Card>
   );
 }
